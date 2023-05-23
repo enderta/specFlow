0f93e5b0b8392e55f73d4898b865cbf509c7249c
enderta <endertanver@gmail.com>
2023-05-23 15:05:06 +0100

    spec

--- SpecFlowProject2/Drivers/Driver.cs
+++ SpecFlowProject2/Drivers/Driver.cs
@@ -1,23 +1,25 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace SpecFlowProject2.Drivers
 {
-  public static class DriverManager
-  {
-    private static WebDriver driver;
-
-    public static WebDriver GetDriver()
+    public static class DriverManager
     {
-      if (driver == null)
-      {
-        driver = new ChromeDriver();
-      }
+        private static WebDriver driver;
 
-      return driver;
+        public static WebDriver GetDriver()
+        {
+            if (driver == null)
+            {
+                new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                driver = new ChromeDriver();
+            }
+
+            return driver;
+        }
     }
-  }
   
   
 }
--- SpecFlowProject2/Hooks/Hook.cs
+++ SpecFlowProject2/Hooks/Hook.cs
@@ -1,16 +1,19 @@
 
+using NUnit.Framework;
 using OpenQA.Selenium;
 using SpecFlowProject2.Drivers;
 
 
 namespace SpecFlowProject2.Hooks
 {
+    
     [Binding]
     public class Hooks
     {
         [BeforeScenario("wip")]
         public void BeforeScenario()
         {
+          
             System.Console.WriteLine("BeforeScenario");
             //wait for one minute
             DriverManager.GetDriver().Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(60);
@@ -23,6 +26,8 @@ namespace SpecFlowProject2.Hooks
         [AfterScenario("wip")]
         public void AfterScenario()
         {
+            //screen shot
+            
             //implicit wait for one minute
             DriverManager.GetDriver().Quit();
         }
